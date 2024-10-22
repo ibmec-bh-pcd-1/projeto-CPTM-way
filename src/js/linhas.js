2792d0ddce10e6548494257a8e1d581acc8a86a2
IanMeirellesDuraes <202402630661@alunos.ibmec.edu.br>
2024-10-22 08:05:51 -0300

    Pequenas mudanças e criação das novas branchs

--- src/js/linhas.js
+++ src/js/linhas.js
@@ -2,39 +2,34 @@
 const metrozinho = document.getElementById("metrozinho");
 const progresso = document.getElementById("Progresso");
 const timerDisplay = document.getElementById("timer");
-const totalTempo = 900; // 15 minutos em segundos (900 segundos)
+const totalTempo = 900; 
 
 function moverMetro() {
-    const progressoWidth = progresso.offsetWidth - metrozinho.offsetWidth; // Largura da barra menos a largura da imagem
-    let tempo = totalTempo; // Inicializa o tempo com 900 segundos
+    const progressoWidth = progresso.offsetWidth - metrozinho.offsetWidth; 
+    let tempo = totalTempo; 
 
     const intervalo = setInterval(() => {
-        // Atualiza o timer
         let minutos = Math.floor(tempo / 60);
         let segundos = tempo % 60;
         timerDisplay.textContent = 
             (minutos < 10 ? "0" + minutos : minutos) + ":" +
             (segundos < 10 ? "0" + segundos : segundos);
 
-        // Calcula a posição atual do metrô proporcional ao tempo
         const posicaoAtual = ((totalTempo - tempo) / totalTempo) * progressoWidth;
 
-        // Move o metrô na barra
         metrozinho.style.left = posicaoAtual + "px";
 
-        // Se o tempo atingir 0, reseta o tempo e o metrô
         if (tempo <= 0) {
-            clearInterval(intervalo); // Para o intervalo
-             // Reinicia
-            tempo = totalTempo; // Reseta o tempo para 15 minutos
-            moverMetro(); // Reinicia o movimento do metrô
+            clearInterval(intervalo);
+            tempo = totalTempo; 
+            moverMetro(); 
         } else {
-            tempo--; // Decrementa o tempo
+            tempo--; 
         }
-    }, 1000); // Atualiza a cada segundo
+    }, 1000); 
 }
 
 window.onload = function() {
-    moverMetro(); // Inicia o movimento do metrô assim que a página carregar
+    moverMetro(); 
 }
     
